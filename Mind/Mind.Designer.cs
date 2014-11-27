2c7e8f4f434cb4d8c85abacabceffcd2355bea16
youthwill <youthwill@yeah.net>
2014-11-27 16:07:59 +0800

    edit
    edit

--- Mind/Mind.Designer.cs
+++ Mind/Mind.Designer.cs
@@ -29,21 +29,197 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mind));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.data_listView = new System.Windows.Forms.ListView();
+            this.qiHao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.kaiJiangHao = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.houEr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.houSan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(784, 562);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "闹着玩的";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mind.Properties.Resources._2014_11_26_16_52_08;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Mind.Properties.Resources._2014_11_26_16_52_08;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Mind.Properties.Resources._2014_11_26_16_52_08;
+            this.pictureBox3.Location = new System.Drawing.Point(121, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Mind.Properties.Resources._2014_11_26_16_52_08;
+            this.pictureBox4.Location = new System.Drawing.Point(172, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Mind.Properties.Resources._2014_11_26_16_52_08;
+            this.pictureBox5.Location = new System.Drawing.Point(223, 20);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.data_listView);
+            this.panel2.Location = new System.Drawing.Point(3, 105);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(781, 447);
+            this.panel2.TabIndex = 1;
+            // 
+            // data_listView
+            // 
+            this.data_listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.qiHao,
+            this.kaiJiangHao,
+            this.houEr,
+            this.houSan});
+            this.data_listView.Location = new System.Drawing.Point(3, 3);
+            this.data_listView.Name = "data_listView";
+            this.data_listView.Size = new System.Drawing.Size(205, 441);
+            this.data_listView.TabIndex = 0;
+            this.data_listView.UseCompatibleStateImageBehavior = false;
+            this.data_listView.View = System.Windows.Forms.View.Details;
+            // 
+            // qiHao
+            // 
+            this.qiHao.Text = "期号";
+            this.qiHao.Width = 36;
+            // 
+            // kaiJiangHao
+            // 
+            this.kaiJiangHao.Text = "开奖号";
+            this.kaiJiangHao.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.kaiJiangHao.Width = 74;
+            // 
+            // houEr
+            // 
+            this.houEr.Text = "后二";
+            this.houEr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.houEr.Width = 36;
+            // 
+            // houSan
+            // 
+            this.houSan.Text = "后三";
+            this.houSan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.houSan.Width = 36;
             // 
             // Mind
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Mind";
             this.Text = "Mind";
+            this.Load += new System.EventHandler(this.Mind_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView data_listView;
+        private System.Windows.Forms.ColumnHeader qiHao;
+        private System.Windows.Forms.ColumnHeader kaiJiangHao;
+        private System.Windows.Forms.ColumnHeader houEr;
+        private System.Windows.Forms.ColumnHeader houSan;
     }
 }
 
--- Mind/Mind.cs
+++ Mind/Mind.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Mind
 {
@@ -15,5 +16,54 @@ namespace Mind
         {
             InitializeComponent();
         }
+
+        public string getDataFromTxt(string path)
+        {
+            string strResult = "";
+            try {
+                StreamReader sr = new StreamReader(path, Encoding.Default);
+                String line = "";
+                int count = 0;
+                while((line=sr.ReadLine())!=null)
+                {
+                    count++;
+                    string qiHao = "";
+                    string kaiJiangHao = "";
+                    string houEr = "";
+                    string houSan = "";
+                    string[] temp = new string[]{""};
+                    
+                    string[] stringSeparators = new string[] { "-" };
+
+                    strResult = line.ToString();
+                    temp = strResult.Split(stringSeparators, StringSplitOptions.None)[1].ToString().Split(new string[] { "\t" }, StringSplitOptions.None);
+                    ListViewItem lvi = new ListViewItem(temp[0].ToString());
+                    lvi.SubItems.AddRange(new string[] { temp[1].ToString() });
+                    this.data_listView.Items.Add(lvi);
+                    if (count == 84)
+                    {
+                        break;
+                    }
+                    //MessageBox.Show(strResult);
+                }
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+            return strResult;
+        }
+
+        public void bindHaoma()
+        {
+            string path = "ssc.txt";
+            //this.data_textBox.Text = getDataFromTxt(path);
+            getDataFromTxt(path);
+        }
+
+        private void Mind_Load(object sender, EventArgs e)
+        {
+            bindHaoma();
+        }
     }
+
 }
